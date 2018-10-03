bf3fd3d4a1f8035b6645f9a968a5071256b6955b
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2018-10-03 01:08:17 -0300

    Segunda aula da terceira matéria

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,57 @@
+var ConnectionFactory = (function () {
+
+  const bd = 'aluraframe';
+  const version = 4;
+  const stores = ['negociacoes'];
+
+  let connection;
+  let close;
+
+  return class ConnectionFactory {
+
+    constructor() {
+      throw new Error('ConnectionFactory não pode ser instanciada');
+    }
+
+    static getConnection() {
+
+      return new Promise((resolve, reject) => {
+        var openRequest = window.indexedDB.open(bd, version);
+
+        openRequest.onupgradeneeded = evento => {
+          ConnectionFactory._createStores(evento.target.result);
+        };
+        openRequest.onsuccess = evento => {
+          if (!connection) {
+            connection = evento.target.result;
+            close = connection.close.bind(connection);
+            connection.close = function(){};
+          }
+          resolve(connection);
+        };
+        openRequest.onerror = evento => {
+          console.log(evento.target.error);
+          reject(evento.target.error.name);
+        };
+      });
+    }
+
+    static closeConnection() {
+      close();
+      connection = null;
+    }
+
+    static _createStores(conn) {
+      stores.forEach(store => {
+        if (conn.objectStoreNames.contains(store)) {
+          conn.deleteObjectStore(store);
+        }
+        conn.createObjectStore(store, { autoIncrement: true });
+      });
+    }
+
+
+  };
+
+
+})();
